1e5705a13aacbd44b794692ad12d1f2fea82703e
martin-creator <martinlubowa@outlook.com>
2023-12-03 12:16:59 +0300

    Added let and const to variables

---
+++ S02/02-let-and-const/02-let-and-const/play.js
@@ -0,0 +1,18 @@
+const name = 'Max';
+let age = 29;
+const hasHobbies = true;
+
+age = 30;
+
+function summarizeUser(userName, userAge, userHasHobby) {
+  return (
+    'Name is ' +
+    userName +
+    ', age is ' +
+    userAge +
+    ' and the user has hobbies: ' +
+    userHasHobby
+  );
+}
+
+console.log(summarizeUser(name, age, hasHobbies));
